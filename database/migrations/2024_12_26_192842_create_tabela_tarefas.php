e5a24fa6872295410d5e9554fd8be52344601ceb
yasminMVieira <minmarques2001@gmail.com>
2024-12-26 16:29:19 -0300

    FEAT: <migrations>: migrations categorias e tarefas

---
+++ database/migrations/2024_12_26_192842_create_tabela_tarefas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo');
+            $table->text('descricao');
+            $table->boolean('completa')->default(false);
+            $table->foreignId('categoria_id')->constrained()->onDelete('cascade'); // Relacionamento com a categoria
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};
